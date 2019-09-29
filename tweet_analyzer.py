048d46f14057e8abef48360777c018e9b9f4bc62
Bill Collins <wcollins113@gmail.com>
2019-09-29 15:55:49 -0400

    Update documentation formatting
    * Update documentation based on suggestions from Pycharm
    * Add return field to function documentation

--- tweet_analyzer.py
+++ tweet_analyzer.py
@@ -6,12 +6,11 @@ CHARACTERS_BETWEEN_DASHES = 6
 SECTION_DIVIDER = "-"
 
 def get_shift_codes(text: str) -> list:
-    """Return a list of strings representing Shift codes from the text parameter.
-    
-    Keyword arguments:
-    text -- text that may or may not include Shift codes.
     """
-
+    Gets possible Shift codes from a string.
+    :param text: String that may or may not include Shift codes.
+    :return: List of strings representing Shift codes from the text parameter.
+    """
     codes = []
     text_clean = text.replace("\n", " ")
     words = text_clean.split(" ")
@@ -21,16 +20,15 @@ def get_shift_codes(text: str) -> list:
     return codes
 
 def is_shift_code(text: str) -> bool:
-    """Returns a boolean representing whether or not the text parameter looks like a Shift code.
+    """
+    Determines if a string looks like it could be a Shift code.
     Qualifications for a proper Shift code:
     * Contains 29 characters
     * Every 6th character is '-'
-    * Does not contain spaces 
-    
-    Keyword arguments:
-    text -- text that may or may not be a Shift code.
+    * Does not contain spaces
+    :param text: String that may or may not be a Shift code.
+    :return: Boolean value representing whether or not the text parameter looks like a Shift code.
     """
-
     if len(text) == SHIFT_CODE_LENGTH and " " not in text:
         sections = text.split(SECTION_DIVIDER)
         if len(sections) == SHIFT_CODE_SECTIONS:
