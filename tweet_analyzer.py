dcec98933d7954f2fc5d8a42b9a26ab2081f83db
Bill Collins <wcollins113@gmail.com>
2019-09-29 21:18:59 -0400

    Add functionality to get a Shift code's possible expiration time to tweet_analyzer
    * Add get_expiration function to get a possible expiration time
    * Add is_time_referencing_word, is_timezone_abbreviation, populate_timezones_list,
      and get_words_from_text as helper functions
    * Add unit tests for all of the functions mentioned above

--- tweet_analyzer.py
+++ tweet_analyzer.py
@@ -4,6 +4,10 @@ FIRST_DASH_INDEX = 5
 LAST_DASH_INDEX = 23
 CHARACTERS_BETWEEN_DASHES = 6
 SECTION_DIVIDER = "-"
+EMPTY_STRING = ""
+TIMEZONES_LIST = list()
+TIMEZONES_FILE = "data/timezones.txt"
+TIME_REFERENCING_WORDS = ["days", "hours", "minutes", "seconds"]
 
 def get_shift_codes(text: str) -> list:
     """
@@ -12,8 +16,7 @@ def get_shift_codes(text: str) -> list:
     :return: List of strings representing Shift codes from the text parameter.
     """
     codes = []
-    text_clean = text.replace("\n", " ")
-    words = text_clean.split(" ")
+    words = get_words_from_text(text)
     for word in words:
         if is_shift_code(word):
             codes.append(word)
@@ -37,3 +40,64 @@ def is_shift_code(text: str) -> bool:
                     return False
             return True
     return False
+
+def get_expiration(text: str) -> str:
+    """
+    Gets the first possible expiration time of a Shift code.
+    :param text: String that may or may not contain an expiration time
+    :return: Possible expiration time. Returns an empty string if no expiration time is found.
+    """
+    words = get_words_from_text(text)
+    for i in range(1, len(words)):
+        word = words[i]
+        if is_timezone_abbreviation(word) or is_time_referencing_word(word):
+            time_string = words[i-1]
+            return time_string + " " + word
+    return EMPTY_STRING
+
+def is_time_referencing_word(text: str) -> bool:
+    """
+    Determines if a string is a time-referencing word. Not case-sensitive.
+    Time referencing words are: days, hours, minutes, seconds.
+    :param text: String that may or may not be a time-referencing word.
+    :return: Boolean value representing whether or not the text parameter is a time-referencing word.
+    """
+    if text.lower() in TIME_REFERENCING_WORDS:
+        return True
+    return False
+
+def is_timezone_abbreviation(text: str) -> bool:
+    """
+    Determines if a string is a timezone abbreviation. Not case-sensitive.
+    :param text: String that may or may not be a timezone abbreviation.
+    :return: Boolean value representing whether or not the text parameter is a timezone abbreviation.
+    """
+    if TIMEZONES_LIST is None or len(TIMEZONES_LIST) == 0:
+        populate_timezones_list()
+    if text.upper() in TIMEZONES_LIST:
+        return True
+    else:
+        return False
+
+def populate_timezones_list():
+    """
+    Populates TIMEZONES_LIST
+    :return: void
+    """
+    with open(TIMEZONES_FILE) as f:
+        text = f.read()
+        timezones = text.split("\n")
+        for timezone in timezones:
+            TIMEZONES_LIST.append(timezone)
+
+def get_words_from_text(text: str) -> list:
+    """
+    Cleans text and retrieves a list of words.
+    :param text: Text string.
+    :return: List of words from the text parameter.
+    """
+    if len(text) == 0:
+        return list()
+    text_clean = text.replace("\n", " ").replace("=", " ")
+    words = text_clean.split(" ")
+    return words
